43ccfe8f56df57517f8028f334b1e71ddf66544a
zrhuan0318 <93692411+zrhuan0318@users.noreply.github.com>
2025-07-24 09:55:36 +0800

    Update sid_tools.py

--- sidnet/sid_tools.py
+++ sidnet/sid_tools.py
@@ -1,61 +1,72 @@
-# sid_tools.py
 import numpy as np
-import torch
+import itertools
+from math import log2
+from collections import defaultdict
 
-def myhistogram(x, nbins):
-    if not isinstance(x, torch.Tensor):
-        x = torch.tensor(x, dtype=torch.float32)
-    if x.ndim == 1:
-        x = x.unsqueeze(1)
-
-    x_min = x.min(dim=0, keepdim=True)[0]
-    x_max = x.max(dim=0, keepdim=True)[0]
-    x_norm = (x - x_min) / (x_max - x_min + 1e-8)
-    x_bins = (x_norm * nbins).long().clamp(max=nbins-1)
-
-    powers = nbins ** torch.arange(x.shape[1]-1, -1, -1)
-    indices = (x_bins * powers).sum(dim=1)
-    hist_flat = torch.bincount(indices, minlength=nbins**x.shape[1]).float()
-    hist = hist_flat.reshape([nbins] * x.shape[1])
-    hist += 1e-14
-    hist /= hist.sum()
-    return hist.numpy()
-
-def mylog(x):
-    valid_indices = (x != 0) & (~np.isnan(x)) & (~np.isinf(x))
-    log_values = np.zeros_like(x)
-    log_values[valid_indices] = np.log2(x[valid_indices])
-    return log_values
+def myhistogram(data, bins):
+    """ Discretize each row of a matrix independently """
+    out = np.zeros_like(data, dtype=int)
+    for i in range(data.shape[0]):
+        out[i] = np.digitize(data[i], np.histogram_bin_edges(data[i], bins=bins)) - 1
+    return out
 
 def entropy(p):
-    return -np.sum(p * mylog(p))
+    p = p[p > 0]
+    return -np.sum(p * np.log2(p))
 
-def entropy_nvars(p, indices):
-    excluded_indices = tuple(set(range(p.ndim)) - set(indices))
-    marginalized_distribution = p.sum(axis=excluded_indices)
-    return entropy(marginalized_distribution)
+def joint_entropy(data):
+    _, counts = np.unique(data, axis=1, return_counts=True)
+    probs = counts / np.sum(counts)
+    return entropy(probs)
 
-def cond_entropy(p, target_indices, conditioning_indices):
-    joint_entropy = entropy_nvars(p, set(target_indices) | set(conditioning_indices))
-    conditioning_entropy = entropy_nvars(p, conditioning_indices)
-    return joint_entropy - conditioning_entropy
+def mutual_information(X, Y):
+    H_X = joint_entropy(X)
+    H_Y = joint_entropy(Y)
+    H_XY = joint_entropy(np.vstack((X, Y)))
+    return H_X + H_Y - H_XY
 
-def mutual_info(p, set1_indices, set2_indices):
-    entropy_set1 = entropy_nvars(p, set1_indices)
-    conditional_entropy = cond_entropy(p, set1_indices, set2_indices)
-    return entropy_set1 - conditional_entropy
+def powerset(iterable, max_comb):
+    "powerset(['a','b','c'], 2) --> (a,) (b,) (c,) (a,b) (a,c) (b,c)"
+    s = list(iterable)
+    return list(itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(1, max_comb+1)))
 
-def cond_mutual_info(p, ind1, ind2, ind3):
-    combined_indices = tuple(set(ind2) | set(ind3))
-    return cond_entropy(p, ind1, ind3) - cond_entropy(p, ind1, combined_indices)
+def compute_all_mi_combinations(Y, nbins=5, max_combs=2):
+    """ Perform SID decomposition: returns I_U, I_R, I_S, MI """
+    nvars = Y.shape[0] - 1  # first row is target
+    X = Y[1:]
+    T = Y[0:1]
+    bins_data = myhistogram(Y, nbins)
 
-def transfer_entropy(p, target_var):
-    num_vars = len(p.shape) - 1
-    TE = np.zeros(num_vars)
-    for i in range(1, num_vars + 1):
-        present_indices = tuple(range(1, num_vars + 1))
-        conditioning_indices = tuple([target_var] + [j for j in range(1, num_vars + 1) if j != i and j != target_var])
-        cond_ent_target_given_past = cond_entropy(p, (0,), conditioning_indices)
-        cond_ent_target_given_past_and_input = cond_entropy(p, (0,), present_indices)
-        TE[i-1] = cond_ent_target_given_past - cond_ent_target_given_past_and_input
-    return TE
+    combs = powerset(range(nvars), max_combs)
+
+    I_R = {}
+    I_S = {}
+    I_U = np.zeros(nvars)
+
+    # Precompute MI for each combination with target
+    mi_with_target = {}
+    for c in combs:
+        key = tuple(sorted(c))
+        joint = np.vstack([bins_data[i+1] for i in key])
+        mi = mutual_information(joint, bins_data[0:1])
+        mi_with_target[key] = mi
+
+    # Compute unique contributions
+    for i in range(nvars):
+        single = (i,)
+        I_U[i] = mi_with_target[single]
+        I_R[single] = I_U[i]
+
+    # Compute redundancy and synergy
+    for c in combs:
+        if len(c) < 2:
+            continue
+        joint_mi = mi_with_target[c]
+        redundant = sum(mi_with_target[(i,)] for i in c)
+        synergy = joint_mi - redundant
+        I_R[c] = redundant
+        I_S[c] = synergy
+
+    MI_total = sum(I_U) + sum(I_S.values())
+
+    return I_U, I_R, I_S, MI_total
